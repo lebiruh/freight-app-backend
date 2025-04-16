5b8e53fdeb3f0c4355af45b61a93417ad4ee43c3
Biruh <lebiruh@yahoo.com>
2025-04-16 13:14:42 +0300

    avatar

--- src/components/Navbar/CustomerNavbar.jsx
+++ src/components/Navbar/CustomerNavbar.jsx
@@ -13,6 +13,7 @@ import "../../i18n"
 
 // import LoginIcon from '@mui/icons-material/Login';
 import { LoginOutlined } from '@mui/icons-material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 
 
@@ -188,13 +189,16 @@ const Navbar = () => {
                 <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
+                  {
+                    customerData ? <AccountCircleIcon/> : <LoginOutlined color='primary' size="40px"/>
+                  }
                   {/* <img
                     alt=""
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                     className="size-8 rounded-full"
                   /> */}
                   {/* <div style={{backgroundColor: 'white', width:'15px', height:'15px', borderRadius: '5px'}}> */}
-                    <LoginOutlined color='primary' size="40px"/>
+                    
                   {/* </div> */}
                   
                 </MenuButton>
